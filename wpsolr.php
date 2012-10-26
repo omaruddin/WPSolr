cc0adde67df7cfdacb2b0f28d97339db9bcf7753
morphatic <registered@morphatic.com>
2012-10-26 11:33:21 -0400

    added solr search engine to the plugin and base directories

---
+++ wpsolr.php
@@ -0,0 +1,10 @@
+<?php
+/*
+Plugin Name: WP Solr
+Plugin URI: http://www.wpsolr.net/
+Description: A WordPress plugin that uses Apache Solr to turn the native WordPress search into a full text search engine capable of indexing all types of documents that are uploaded to the site.
+Version: 0.1.0
+Author: Morgan Benton and Rachel Jacobson
+Author URI: http://www.wpsolr.net/
+License: GPLv3
+*/
