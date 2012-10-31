f32451361817d30a037f14d9a823ac249df3894e
morphatic <registered@morphatic.com>
2012-10-31 12:10:48 -0400

    added a filter to add a metadata field to the media form

--- wpsolr.php
+++ wpsolr.php
@@ -10,7 +10,7 @@ License: GPLv3
 */
 
 // check to see that the WPSolr class does not already exist
-if ( ! class_exists( 'WPSolr' ) ) :
+if ( ! class_exists( 'WPSolr' ) ) {
 
 // if not, then create it
 class WPSolr {
@@ -19,14 +19,40 @@ class WPSolr {
 	 * Constructor
 	 */
 	function WPSolr() {
+		add_action( 'wp_loaded', array( &$this, 'wp_loaded' ) );
 	}
 	// old-style constructor for backward PHP compatibility
 	function __construct() { $this->WPSolr(); }
+	
+	function wp_loaded() {
+	
+		// add filters
+		add_filter( 'attachment_fields_to_edit', array( &$this, 'attachment_fields_to_edit' ), null, 2 );
+		add_filter( 'attachment_fields_to_save', array( &$this, 'attachment_fields_to_save' ), null, 2 );
+	}
+	
+	/**
+	 * Modifies the list of fields available when editing uploaded media files
+	 */
+	function attachment_fields_to_edit( $fields, $post ) {
+		$fields[ 'wpsolr' ][ 'label' ] = __( 'WPSolr Field' );
+		$fields[ 'wpsolr' ][ 'value' ] = get_post_meta( $post->ID, "wpsolr", true );
+		$fields[ 'wpsolr' ][ 'helps' ] = __( 'Here is some helpful text.' );
+		return $fields;
+	}
+	
+	/**
+	 * Saves the data from the fields added by attachment_fields_to_edit
+	 */
+	function attachment_fields_to_save() {
+	
+	}
+	
 }
 // then create an instance of the class
 if ( ! isset( $wpsolr ) ) $wpsolr = new WPSolr;
 
-else:
+} else {
 	// WPSolr class already existed
 	die( 'WPSolr class already exists!' );
-endif;
+}
